42be2d9baed8092cadaf970138a1b4a68da89275
Natalio <natodjandji>
2026-03-22 22:19:25 -0400

    INICIO

---
+++ src/proyecto2natalio_vera/Proyecto2Natalio_Vera.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto2natalio_vera;
+
+/**
+ *
+ * @author natalio
+ */
+public class Proyecto2Natalio_Vera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

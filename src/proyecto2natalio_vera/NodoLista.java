9021d9be9628a681c9326708e36db67a496e0508
Natalio <natodjandji>
2026-03-22 22:48:22 -0400

    CLASE NODOLISTA

---
+++ src/proyecto2natalio_vera/NodoLista.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2natalio_vera;
+
+/**
+ *
+ * @author natalio
+ */
+class NodoLista<T> {
+    T data;
+    NodoLista<T> next;
+    
+    public NodoLista(T data) {
+        this.data = data;
+    }
+}

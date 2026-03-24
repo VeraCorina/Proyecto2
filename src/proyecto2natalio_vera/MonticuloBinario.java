0faea115e43a2d02d4e90d2e50edace409c15762
coco <VeraCorina>
2026-03-23 23:26:03 -0400

    Monticulo Binario metodos 1

--- src/proyecto2natalio_vera/MonticuloBinario.java
+++ src/proyecto2natalio_vera/MonticuloBinario.java
@@ -16,4 +16,17 @@ class MonticuloBinario {
         heap = new RegistroImpresion[capacidad];
         size = 0;
     }
+    
+    private int padre(int i) { 
+        return (i - 1) / 2; 
+    }
+    
+    private int hijoIzq(int i) { 
+        return 2 * i + 1;
+    }
+    
+    private int hijoDer(int i) { 
+        return 2 * i + 2; 
+    }
+    
 }

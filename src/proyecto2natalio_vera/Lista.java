d349cfa67bcab745029a8098502df0ae370750a7
coco <VeraCorina>
2026-03-25 15:31:19 -0400

    JavaDoc 5

--- src/proyecto2natalio_vera/Lista.java
+++ src/proyecto2natalio_vera/Lista.java
@@ -5,13 +5,18 @@
 package proyecto2natalio_vera;
 
 /**
- *
+ * Clase de lista enlasada simple pa guardar cualquier cosa genérica.
+ * @param <T> El tipo de dato que va a guardar la lista.
  * @author natalio
  */
 class Lista<T> {
     NodoLista<T> head;
     int size;
     
+    /**
+     * Agrega un elemento al final de la lista.
+     * @param data El objeto que queremos meter.
+     */
     public void add(T data) {
         NodoLista<T> nuevo = new NodoLista<>(data);
         if (head == null) {
@@ -26,6 +31,11 @@ class Lista<T> {
         size++;
     }
     
+    /**
+     * Busca un elemento por su posicion en la lista.
+     * @param index Numero del indice (empiesa en 0).
+     * @return El objeto en esa posicion.
+     */
     public T get(int index) {
         NodoLista<T> current = head;
         for (int i = 0; i < index; i++) {
@@ -34,6 +44,10 @@ class Lista<T> {
         return current.data;
     }
     
+    /**
+     * Quita un elemento de la lista comparando el objeto.
+     * @param data El objeto que se quiere borrar.
+     */
     public void remove(T data) {
         if (head == null) return;
         if (head.data.equals(data)) {
@@ -51,6 +65,9 @@ class Lista<T> {
         }
     }
     
+    /**
+     * @return Cuantos elementos hay en la lista ahora mismo.
+     */
     public int size() {
         return size;
     }
